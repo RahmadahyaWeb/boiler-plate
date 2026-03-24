b75f61c9e8fc6692a481781c91d39677d88ce066
rahmadahyaWeb <thelastpc24@gmail.com>
2026-03-24 20:14:31 +0800

    fix

--- config/sidebar.php
+++ config/sidebar.php
@@ -15,18 +15,33 @@ return [
         'children' => [
 
             [
+                'label' => 'Roles & Permissions',
+                'icon' => 'lock-closed',
+                'route' => 'access-control.roles.index',
+                'permission' => 'roles.view',
+            ],
+
+        ],
+    ],
+
+    [
+        'label' => 'User Management',
+        'icon' => 'users',
+        'children' => [
+
+            [
                 'label' => 'Users',
                 'icon' => 'users',
                 'route' => 'users.index',
                 'permission' => 'users.view',
             ],
 
-            [
-                'label' => 'Roles',
-                'icon' => 'lock-closed',
-                'route' => 'roles.index',
-                'permission' => 'roles.view',
-            ],
+            // [
+            //     'label' => 'User Devices',
+            //     'icon' => 'device-phone-mobile',
+            //     'route' => 'users.devices.index',
+            //     'permission' => 'user_devices.view',
+            // ],
 
         ],
     ],
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,30 @@ Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 
-    Route::livewire('users', 'pages::users.index')->name('users.index');
-    Route::livewire('roles', 'pages::roles.index')->name('roles.index');
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESS CONTROL
+    |--------------------------------------------------------------------------
+    */
+
+    Route::prefix('access-control')->name('access-control.')->group(function () {
+
+        Route::livewire('roles', 'pages::roles.index')->name('roles.index');
+
+    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | USER MANAGEMENT
+    |--------------------------------------------------------------------------
+    */
+
+    Route::prefix('users')->name('users.')->group(function () {
+
+        Route::livewire('/', 'pages::users.index')->name('index');
+        // Route::livewire('devices', 'pages::user-devices.index')->name('devices.index');
+
+    });
 });
 
 require __DIR__.'/settings.php';
